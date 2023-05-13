a539c67078cc910cbf78bf8f3a8f635cd66d20e4
FCBleomessi10 <710823459@qq.com>
2023-05-13 22:20:17 +0800

    结构型模式-外观模式

---
+++ design/src/main/java/pattern/facade/SmartAppliancesFacade.java
@@ -0,0 +1,35 @@
+package pattern.facade;
+
+public class SmartAppliancesFacade {
+    private Light light;
+    private TV tv;
+    private AirCondition airCondition;
+
+    public SmartAppliancesFacade() {
+        light = new Light();
+        tv = new TV();
+        airCondition = new AirCondition();
+    }
+
+    public void say(String message) {
+        if (message.contains("打开")) {
+            on();
+        } else if (message.contains("关闭")) {
+            off();
+        } else {
+            System.out.println("无");
+        }
+    }
+
+    private void on() {
+        light.on();
+        tv.on();
+        airCondition.on();
+    }
+
+    private void off() {
+        light.off();
+        tv.off();
+        airCondition.off();
+    }
+}
